3b3a6da8521f049ad188793a569a737baf160b09
Lobsterge <97110278+Lobsterge@users.noreply.github.com>
2023-06-05 12:33:27 +0200

    Add files via upload

--- evaluator.js
+++ evaluator.js
@@ -8,6 +8,106 @@ const answerButtonsElement = document.getElementById('answer-buttons')
 
 let shuffledQuestions, currentQuestionIndex
 
+const var questions = [
+  {
+    question: "Dove si trova il santuario?",
+    answers:[
+      {text: "Concesio", correct:false},
+      {text: "Collebeato", correct:true},
+      {text: "Cellatica", correct:false},
+      {text: "Gussago", correct:false},
+    ]
+  },
+
+  {
+    question: "Secolo della costruzione?",
+    answers:[
+      {text: "VII", correct:false},
+      {text: "IX", correct:true},
+      {text: "XI", correct:false},
+      {text: "XVII", correct:false},
+    ]
+  },
+
+  {
+    question: "Cos'era prima?",
+    answers:[
+      {text: "Monastero", correct:true},
+      {text: "Chiesa", correct:false},
+      {text: "Comune", correct:false},
+      {text: "Palazzo", correct:false},
+    ]
+  },
+
+  {
+    question: "Di che tipo di architettura e'?",
+    answers:[
+      {text: "Greca", correct:false},
+      {text: "Romanica", correct:true},
+    ]
+  },
+
+  {
+    question: "Di che tipo è la flora?",
+    answers:[
+      {text: "Montana", correct:false},
+      {text: "Mediterranea", correct:false},
+      {text: "Collinare", correct:true},
+      {text: "Tropicale", correct:false},
+    ]
+  },
+
+  {
+    question: "Che animali sono più comuni?",
+    answers:[
+      {text: "Caprioli", correct:false},
+      {text: "Cervi", correct:false},
+      {text: "Lupi", correct:false},
+      {text: "Lepri", correct:true},
+    ]
+  },
+
+{
+    question: "Cosa narra la leggenda?",
+    answers:[
+      {text: "Fondato da San Colombano", correct:true},
+      {text: "Fondato da Santo Stefano", correct:false},
+      {text: "Presenza di un fantasma", correct:false},
+      {text: "Presenza di animali magici", correct:false},
+    ]
+  },
+
+  {
+    question: "Com'è il clima?",
+    answers:[
+      {text: "Arido", correct:false},
+      {text: "Nevoso", correct:false},
+      {text: "Contintentale", correct:true},
+      {text: "Tropicale", correct:false},
+    ]
+  },
+
+  {
+    question: "Dove si trova Collebeato?",
+    answers:[
+      {text: "Brescia", correct:true},
+      {text: "Bergamo", correct:false},
+      {text: "Monza", correct:false},
+      {text: "Concesio", correct:false},
+    ]
+  },
+
+  {
+    question: "Che animali sono più comuni?",
+    answers:[
+      {text: "Caprioli", correct:false},
+      {text: "Cervi", correct:false},
+      {text: "Lupi", correct:false},
+      {text: "Lepri", correct:true},
+    ]
+  }
+];
+
 startButton.addEventListener('click', startGame)
 nextButton.addEventListener('click', () => {
   currentQuestionIndex++
@@ -78,46 +178,3 @@ function clearStatusClass(element) {
   element.classList.remove('wrong')
 }
 
-const questions = [
-  {
-    question: 'Domanda 1',
-    answers: [
-      { text: 'Risposta 1', correct: true },
-      { text: 'Risposta 2', correct: false }
-    ]
-  },
-  {
-    question: 'Domanda 2',
-    answers: [
-      { text: 'Risposta 1', correct: true },
-      { text: 'Risposta 2', correct: true },
-      { text: 'Risposta 3', correct: true },
-      { text: 'Risposta 4', correct: true }
-    ]
-  },
-  {
-    question: 'Domanda 3',
-    answers: [
-      { text: 'Risposta 1', correct: false },
-      { text: 'Risposta 2', correct: true },
-      { text: 'Risposta 3', correct: false },
-      { text: 'Risposta 4', correct: false }
-    ]
-  },
-  {
-    question: 'Domanda 4',
-    answers: [
-      { text: 'Risposta 1', correct: false },
-      { text: 'Risposta 2', correct: true }
-    ]
-  },
-  {
-    question: 'Domanda 5',
-  answers: [
-    { text: 'Risposta 1', correct: false },
-    { text: 'Risposta 2', correct: false },
-    { text: 'Risposta 3', correct: false },
-    { text: 'Risposta 4', correct: true }
-  ]
-}
-]   
